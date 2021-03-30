e9aac3af8ac88475d5e4fbc0ba63635d52c5db43
Bob Stauss <robertrstauss@gmail.com>
2021-03-30 09:30:05 -0400

    working email/pass auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import firebase from "firebase/app";
+import "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCJGRvSo2zhLjgJn7pZ7oCKuFHraXDQ-34",
+  authDomain: "example-auth-5e946.firebaseapp.com",
+  databaseURL: "https://example-auth-5e946.firebaseio.com",
+  projectId: "example-auth-5e946",
+  storageBucket: "example-auth-5e946.appspot.com",
+  messagingSenderId: "111109385265",
+  appId: "1:111109385265:web:e404cc2ae5d06b2d3b7910",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  function loginPass(email, password) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((userObj) => {
+        // Signed in
+        setUser(userObj)
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Link to="/login">Login</Link>
+      </div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(props) => {
+            return <Dashboard user={user} />;
+          }}
+        />
+        <Route
+          path="/login"
+          render={(props) => {
+            return <Login loginPass={loginPass} user={user}/>;
+          }}
+        />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import {useState} from 'react'
+import {Link} from 'react-router-dom'
+
+function Login(props) {
+
+  const [emailVal, setEmailVal] = useState('')
+  const [passVal, setPassVal] = useState('')
+
+  function handleInput(evt) {
+    if(evt.target.type === 'email') {
+      setEmailVal(evt.target.value)
+    } else {
+      setPassVal(evt.target.value)
+    }
+  }
+
+  return (
+    <div>
+      <h3>Please Login with email and password</h3>
+      <form onSubmit={(evt) => {
+        evt.preventDefault();
+        props.loginPass(emailVal, passVal)
+      }}>
+        <input type='email' value={emailVal} onChange={handleInput} />
+        <input type='password' value={passVal} onChange={handleInput} />
+        <input type='submit' />
+      </form>
+      <button>Login with Google</button>
+      {props.user && <Link to='/' >Go to Dashboard</Link>}
+    </div>
+  )
+}
+
+export default Login
